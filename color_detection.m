949d81a924e74db017d9275993c29bec2a3efc46
Gonzalez Bueno <jg5915@ic.ac.uk>
2016-11-08 11:47:38 +0000

    labels with colors and centroids plot

--- color_detection.m
+++ color_detection.m
@@ -9,19 +9,6 @@ im_g = image(:,:,2);
 im_b = image(:,:,3);
 
 %% take off the white background (if any)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% % histograms of the three channels
-% [countsR,binLocationsR] = imhist(im_r);
-% [countsG,binLocationsG] = imhist(im_g);
-% [countsB,binLocationsB] = imhist(im_b);
-% 
-% % get the maximum number of pixels with one same value in each channel
-% Rmax = max(countsR);
-% Gmax = max(countsG);
-% Bmax = max(countsB);
-% 
-
 % Initialise 
 noBack_r = im_r;
 noBack_g = im_g;
@@ -74,35 +61,82 @@ blueMask = imerode(blueMask , strel('diamond', 2));
 blueMask = imdilate(blueMask , strel('diamond', 3));
 blueMask = bwareafilt(blueMask ,[2000 200000]);
 
-% Labeling
-% [LR,numR] = bwlabel(redMask);
-% [LG,numG] = bwlabel(greenMask);
-% [LB,numB] = bwlabel(blueMask);
-
-sR = regionprops(redMask,'centroid');
-centroidsR = cat(1, sR.Centroid);
-
 
 %% Reconstruction
 
-%reconstruct red image
-imageR(:,:,1) = uint8(redMask) .* noBack_r;
-imageR(:,:,2) = uint8(redMask) .* noBack_g;
-imageR(:,:,3) = uint8(redMask) .* noBack_b;
+% %reconstruct red image
+% imageR(:,:,1) = uint8(redMask) .* noBack_r;
+% imageR(:,:,2) = uint8(redMask) .* noBack_g;
+% imageR(:,:,3) = uint8(redMask) .* noBack_b;
+% 
+% %reconstruct green image
+% imageG(:,:,1) = uint8(greenMask) .* noBack_r;
+% imageG(:,:,2) = uint8(greenMask) .* noBack_g;
+% imageG(:,:,3) = uint8(greenMask) .* noBack_b;
+% 
+% %reconstruct blue image
+% imageB(:,:,1) = uint8(blueMask) .* noBack_r;
+% imageB(:,:,2) = uint8(blueMask) .* noBack_g;
+% imageB(:,:,3) = uint8(blueMask) .* noBack_b;
 
-%reconstruct green image
-imageG(:,:,1) = uint8(greenMask) .* noBack_r;
-imageG(:,:,2) = uint8(greenMask) .* noBack_g;
-imageG(:,:,3) = uint8(greenMask) .* noBack_b;
+% reconstruct an image with all the markers
+allMask = uint8(redMask | greenMask | blueMask);
+all(:,:,1) = uint8(allMask) .* noBack_r;
+all(:,:,2) = uint8(allMask) .* noBack_g;
+all(:,:,3) = uint8(allMask) .* noBack_b;
 
-%reconstruct blue image
-imageB(:,:,1) = uint8(blueMask) .* noBack_r;
-imageB(:,:,2) = uint8(blueMask) .* noBack_g;
-imageB(:,:,3) = uint8(blueMask) .* noBack_b;
+%% Labeling and finding centroids
 
-figure, imshow(imageR), 
+% label the image
+[L,num] = bwlabel(allMask);
+
+% get the centroids
+s = regionprops(L,'centroid');
+centroids = cat(1, s.Centroid);
+
+% See the colors of the centroids
+colors = impixel (all, centroids(:,1), centroids(:,2));
+
+% go through all the centroids and check to what color they correspond
+% create a matrix with 
+%   - as many rows as labels there are
+%   - 4 columns (one for the label number, 2 for the centroid pos, 1 for the
+%   color).
+listIndex = zeros(num,4);
+for i = 1:num
+    % LABEL NAME
+    listIndex (i,1) = i;
+    % CENTROID POSITION
+    listIndex (i,2) = centroids(i, 1);
+    listIndex (i,3) = centroids(i, 2);
+    % COLOR
+    % if not all 3 channels are 0
+    if (colors(i,1) ~= 0 || colors(i,2) ~= 0 || colors(i,3) ~= 0)
+       [M,I] = max(colors(i,:));
+       listIndex(i,4) = I;
+    end
+end
+
+
+%% Ploting
+figure, imshow (image);
 hold on
-plot(centroidsR(:,1),centroidsR(:,2), 'b*')
-hold off
-figure, imshow(imageG);
-figure, imshow(imageB);
+for i = 1:length(listIndex)
+    if (listIndex(i,4)~= 0)
+        % plot red
+        if (listIndex(i,4) == 1)
+        plot (listIndex(i,2), listIndex(i,3),'r*')
+        hold on
+        end
+        % plot green
+        if (listIndex(i,4) == 2)
+        plot (listIndex(i,2), listIndex(i,3),'g*')
+        hold on
+        end
+        % plot blue
+        if (listIndex(i,4) == 3)
+        plot (listIndex(i,2), listIndex(i,3),'b*')
+        hold on
+        end
+    end
+end
