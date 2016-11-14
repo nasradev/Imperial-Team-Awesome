fd051a4d47ba356ab89d522fa80d7855c40c1b89
Gonzalez Bueno <jg5915@ic.ac.uk>
2016-11-14 13:43:59 +0000

    run video_test to try the code. getMarkerPos is a function that gives back the markers position

--- color_detection.m
+++ color_detection.m
@@ -1,21 +1,17 @@
+function [markersPos] = getMarkersPos(image)
 clear;
 close all;
-% Load the image
-image = imread('IMG_5573.JPG');
-
-% Separate in three RGB channels
-im_r = image(:,:,1);
-im_g = image(:,:,2);
-im_b = image(:,:,3);
+% % Load the image
+% image = imread('IMG_5575.JPG');
 
 %% take off the white background (if any)
-% Initialise 
-noBack_r = im_r;
-noBack_g = im_g;
-noBack_b = im_b;
+% Initialise
+noBack_r = image(:,:,1);
+noBack_g = image(:,:,2);
+noBack_b = image(:,:,3);
 for i = 1:length(image(:,1,1))
     for j = 1:length(image(1,:,1))
-        if (im_r(i,j) > 150 && im_g(i,j) > 150 && im_b(i,j) > 150)
+        if (noBack_r(i,j) > 150 && noBack_g(i,j) > 150 && noBack_b(i,j) > 150)
             noBack_r(i,j) = 0;
             noBack_g(i,j) = 0;
             noBack_b(i,j) = 0;
@@ -23,49 +19,29 @@ for i = 1:length(image(:,1,1))
     end
 end
 
-%% Thresholding 
-
+%% Thresholding
 %Threshold red
-% redThresholdLow = 120;%
-% redThresholdHigh = 255;
-% redMask = (noBack_r >= redThresholdLow) & (noBack_r <= redThresholdHigh);
 levelR = graythresh(noBack_r);
 redMask = im2bw(noBack_r, levelR);
 
 %Threshold green
-% greenThresholdLow = 120;
-% greenThresholdHigh = 255;
-% greenMask = (noBack_g >= greenThresholdLow) & (noBack_g <= greenThresholdHigh);
 levelG = graythresh(noBack_g);
 greenMask = im2bw(noBack_g, levelG);
 
 %Threshold blue
-% blueThresholdLow = 120;
-% blueThresholdHigh = 255;
-% blueMask = (noBack_b >= blueThresholdLow) & (noBack_b <= blueThresholdHigh);
 levelB = graythresh(noBack_b);
 blueMask = im2bw(noBack_b, levelB);
 
+% create a mask of the three colors
+allMask = redMask | greenMask | blueMask;
+
+%erode the mask
+allMask = imerode(allMask, strel('diamond',3));
+
 % Get rid of small objects.
-% First an erosion, then only keep objects between 900 and 5000 px and
-% finally a dilatation
-redMask = imerode(redMask, strel('diamond', 2));
-redMask = imdilate(redMask, strel('diamond', 3));
-redMask = bwareafilt(redMask,[2000 200000]);
- 
-greenMask = imerode(greenMask , strel('diamond', 2));
-greenMask = imdilate(greenMask , strel('diamond', 3));
-greenMask = bwareafilt(greenMask ,[2000 200000]);
- 
-blueMask = imerode(blueMask , strel('diamond', 2));
-blueMask = imdilate(blueMask , strel('diamond', 3));
-blueMask = bwareafilt(blueMask ,[2000 200000]);
-
-
-% Reconstruction
+allMask = bwareafilt(allMask ,[2000 200000]);
 
 % reconstruct an image with all the markers
-allMask = uint8(redMask | greenMask | blueMask);
 all(:,:,1) = uint8(allMask) .* noBack_r;
 all(:,:,2) = uint8(allMask) .* noBack_g;
 all(:,:,3) = uint8(allMask) .* noBack_b;
@@ -76,18 +52,15 @@ all(:,:,3) = uint8(allMask) .* noBack_b;
 [L,num] = bwlabel(allMask);
 
 % get the centroids
-s = regionprops(L,'centroid', 'orientation');
+s = regionprops(L,'centroid', 'orientation', 'BoundingBox');
 centroids = cat(1, s.Centroid);
 
-% See the colors of the centroids
-colors = impixel (all, centroids(:,1), centroids(:,2));
-
 % go through all the centroids and check to what color they correspond
-% create a matrix with 
+% create a matrix with
 %   - as many rows as labels there are
-%   - 4 columns (one for the label number, 2 for the centroid pos, 1 for the
-%   color).
-listIndex = zeros(num,4);
+%   - 5 columns (one for the label number, 2 for the centroid pos, 1 for the
+%   color, 1 for the ratio width/height).
+listIndex = zeros(num,5);
 for i = 1:num
     % LABEL NAME
     listIndex (i,1) = i;
@@ -96,9 +69,14 @@ for i = 1:num
     listIndex (i,3) = centroids(i, 2);
     % COLOR
     % if not all 3 channels are 0
-    if (colors(i,1) ~= 0 || colors(i,2) ~= 0 || colors(i,3) ~= 0)
-       [M,I] = max(colors(i,:));
-       listIndex(i,4) = I;
+    % See the colors of the centroids
+    color = impixel (all, centroids(i,1), centroids(i,2));
+    if (color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0)
+        [MaxColor,Index] = max(color);
+        orderedColors = sort(color);
+        if (orderedColors(3)-orderedColors(2) > 10)
+            listIndex(i,4) = Index;
+        end
     end
 end
 
@@ -106,29 +84,64 @@ end
 % find the orientation of the labels
 angles = cat(1, s.Orientation);
 
-% find edges along that direction near the centroid
+% compute de bounding box
+bb = cat(1, s.BoundingBox);
 
-% compare
+% find edges along that direction near the centroid
+for i = 1:num
+    if (listIndex(i,4)~= 0)
+        tinyLabel = imcrop(L ,bb(i,:));
+        tinyLabel = imrotate(tinyLabel, -angles(i));
+        %figure, imshow(tinyLabel);
+        
+        % label the image
+        [Ltemp,numTemp] = bwlabel(tinyLabel);
+        
+        % get the centroids
+        sTemp = regionprops(Ltemp,'BoundingBox');
+        
+        % compute de bounding box
+        bbTemp = cat(1, sTemp.BoundingBox);
+        
+        % calculate ratio
+        listIndex(i,5) = bbTemp(1,3) / bbTemp(1,4);
+    end
+end
 
 %% Ploting
 figure, imshow (image);
 hold on
 for i = 1:length(listIndex)
-    if (listIndex(i,4)~= 0)
+    % Stablish criteria:
+    % if the centroid is inside the marker
+    % if the ratio width/heigth > 2.8
+    % if the ratio width/heigth < 4.3
+    if (listIndex(i,4)~= 0 && (2.5 < listIndex(i,5)) && (listIndex(i,5) < 4.3))
         % plot red
         if (listIndex(i,4) == 1)
-        plot (listIndex(i,2), listIndex(i,3),'r*')
-        hold on
+            plot (listIndex(i,2), listIndex(i,3),'r*')
+            hold on
         end
         % plot green
         if (listIndex(i,4) == 2)
-        plot (listIndex(i,2), listIndex(i,3),'g*')
-        hold on
+            plot (listIndex(i,2), listIndex(i,3),'g*')
+            hold on
         end
         % plot blue
         if (listIndex(i,4) == 3)
-        plot (listIndex(i,2), listIndex(i,3),'b*')
-        hold on
+            plot (listIndex(i,2), listIndex(i,3),'b*')
+            hold on
         end
     end
+end
+
+markersPos = [];
+for i = length(listIndex)
+    if (listIndex(i,4)~= 0 ...
+            && (2.5 < listIndex(i,5)) ...
+            && (listIndex(i,5) < 4.3))
+        numberMarkers(i,1) = listIndex(i,2);
+        numberMarkers(i,2) = listIndex(i,3);
+    end
+end
 end
---
+++ getMarkersPos.m
@@ -0,0 +1,176 @@
+function [markersPos] = getMarkersPos(image)
+close all;
+
+% Load the image (uncomment if not using a video)
+%image = imread(image);
+
+%% Image processing
+% take off the white background (if any)-----------------------------------
+% Initialise
+noBack_r = image(:,:,1);
+noBack_g = image(:,:,2);
+noBack_b = image(:,:,3);
+for i = 1:length(image(:,1,1))
+    for j = 1:length(image(1,:,1))
+        if (noBack_r(i,j) > 160 ...
+                && noBack_g(i,j) > 160 ...
+                && noBack_b(i,j) > 160)
+            noBack_r(i,j) = 0;
+            noBack_g(i,j) = 0;
+            noBack_b(i,j) = 0;
+        end
+    end
+end
+
+% Generation of a mask ----------------------------------------------------
+%Threshold red
+levelR = graythresh(noBack_r);
+redMask = im2bw(noBack_r, levelR);
+
+%Threshold green
+levelG = graythresh(noBack_g);
+greenMask = im2bw(noBack_g, levelG);
+
+%Threshold blue
+levelB = graythresh(noBack_b);
+blueMask = im2bw(noBack_b, levelB);
+
+% aprox length of the long side
+imageSizeCM = 25;
+markerSize = length(image(:,1))/imageSizeCM;
+% area = long side * short side (1/4 of the long one)
+markerArea = markerSize * markerSize /4;
+
+% Get rid of small objects.
+redMask = bwareafilt(redMask , [markerArea/2 inf]);%[2000 200000]);
+greenMask = bwareafilt(greenMask , [markerArea/2 inf]);
+blueMask = bwareafilt(blueMask , [markerArea/2 inf]);
+
+% create a mask of the three colors
+allMask = redMask | greenMask | blueMask;
+
+%erode the mask
+allMask = imerode(allMask, strel('diamond',3));
+
+% reconstruct an image with all the markers
+all(:,:,1) = uint8(allMask) .* noBack_r;
+all(:,:,2) = uint8(allMask) .* noBack_g;
+all(:,:,3) = uint8(allMask) .* noBack_b;
+
+%% Labeling and identification
+
+% label the image
+[L,num] = bwlabel(allMask);
+
+% get the centroids
+s = regionprops(L,'centroid', 'orientation', 'BoundingBox', 'area');
+centroids = cat(1, s.Centroid);
+
+% go through all the centroids and check to what color they correspond
+% create a matrix with
+%   - as many rows as labels there are
+%   - 5 columns (one for the label number, 2 for the centroid pos, 1 for the
+%   color, 1 for the ratio width/height).
+listIndex = zeros(num,4);%5);
+for i = 1:num
+    % LABEL NAME
+    listIndex (i,1) = i;
+    % CENTROID POSITION
+    listIndex (i,2) = centroids(i, 1);
+    listIndex (i,3) = centroids(i, 2);
+    % COLOR
+    % if not all 3 channels are 0
+    % See the colors of the centroids
+    color = impixel (all, centroids(i,1), centroids(i,2));
+    %if (color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0)
+        [MaxColor,Index] = max(color);
+        %orderedColors = sort(color);
+        %if (orderedColors(3)-orderedColors(2) > 10)
+            listIndex(i,4) = Index;
+        %end
+    %end
+end
+
+%% Feature filtering
+% find the orientation of the labels
+angles = cat(1, s.Orientation);
+
+% compute de bounding box
+bb = cat(1, s.BoundingBox);
+
+% find edges along that direction near the centroid
+% for i = 1:num
+%     if (listIndex(i,4)~= 0)
+%         tinyLabel = imcrop(L ,bb(i,:));
+%         tinyLabel = imrotate(tinyLabel, -angles(i));
+%         %figure, imshow(tinyLabel);
+%         
+%         % label the image
+%         [Ltemp,numTemp] = bwlabel(tinyLabel);
+%         
+%         % get the bounding boxes
+%         sTemp = regionprops(Ltemp,'BoundingBox');
+%         
+%         % create an array where the ratios between the sides of the
+%         % bounding boxes will be stored
+%         ratios = zeros(1,numTemp);
+%         
+%         % for all the regions in the tiny image
+%         for j = 1:numTemp
+%             % compute de bounding box
+%             bbTemp = cat(1, sTemp.BoundingBox);
+%             
+%             % calculate ratio
+%             ratios(j) = bbTemp(j,3) ./ bbTemp(j,4);
+%         end
+%         
+%         % take the bigger
+%         listIndex(i,5) = max(ratios);
+%     end
+% end
+
+%% Ploting and giving output
+figure, imshow (image);
+hold on
+for i = 1:num
+    % establish criteria:
+    % - if the centroid is inside the marker
+    % - if the ratio width/heigth > 2.8
+    % - if the ratio width/heigth < 4.3
+    if (listIndex(i,4)~= 0 )%...
+%             && (2.5 < listIndex(i,5)) ...
+%             && (listIndex(i,5) < 4.3))
+        % plot red
+        if (listIndex(i,4) == 1)
+            plot (listIndex(i,2), listIndex(i,3),'r*')
+            hold on
+        end
+        % plot green
+        if (listIndex(i,4) == 2)
+            plot (listIndex(i,2), listIndex(i,3),'g*')
+            hold on
+        end
+        % plot blue
+        if (listIndex(i,4) == 3)
+            plot (listIndex(i,2), listIndex(i,3),'b*')
+            hold on
+        end
+    end
+end
+
+% Output: markersPosition -------------------------------------------------
+markersPos = zeros(num,2);
+for i = 1:num
+    if (listIndex(i,4)~= 0 )%...
+%             && (2.5 < listIndex(i,5)) ...
+%             && (listIndex(i,5) < 4.3))
+        markersPos(i,1) = listIndex(i,2);
+        markersPos(i,2) = listIndex(i,3);
+    end
+end
+
+% figure, imshow(image)
+% hold on
+% plot(centroids(:,1),centroids(:,2),'r*')
+% hold off
+end
