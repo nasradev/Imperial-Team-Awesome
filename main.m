3337fba2226ca3282e61f7edb3e569a42b856539
Radev <anr16@ic.ac.uk>
2016-11-28 18:03:17 +0000

    Added handler for Nairouzs code for marker detections

--- main.m
+++ main.m
@@ -16,12 +16,17 @@ vidHeight = obj.Height;
 mov = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'), 'colormap',[]);
 k = 1;
 
-%Go through the video frames
+% Go through the video frames
 while hasFrame(obj);  
     data = readFrame(obj);
+    % Get the checkerboards:
     [imagePoints, worldPoints, colour, threePoints] = ...
         FindCheckerboardPattern( data, squareSize );
-                        
+    
+    % Get the marker positions:
+    [redM, yellowM, greenM, blueM] = getMarkerPos(obj);
+    %Red and Yellow in tool 1, Green and Blue in tool 2
+    
 end %hasFrame
 
 %Output the results to video:
