1c0c5e5c196639011c5861370adb2dc9906baf21
Radev <anr16@ic.ac.uk>
2016-11-28 17:57:45 +0000

    Added functions to detect checkerboards and output required information + started main.m

---
+++ main.m
@@ -0,0 +1,33 @@
+%main code
+function main()
+
+%%Definitions
+
+%Size of checkerboard squares
+squareSize = 5.4;
+
+%Load up the camera parameters:
+load('naskosCameraParamsLaptop.mat') %or whatever
+
+%Set the video file and define output video object
+obj = VideoReader('C:\Users\anr16\SharePoint\Gonzalez Bueno, Juana\Group project\Aurora videos\6.MOV');
+vidWidth = obj.Width;
+vidHeight = obj.Height;
+mov = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'), 'colormap',[]);
+k = 1;
+
+%Go through the video frames
+while hasFrame(obj);  
+    data = readFrame(obj);
+    [imagePoints, worldPoints, colour, threePoints] = ...
+        FindCheckerboardPattern( data, squareSize );
+                        
+end %hasFrame
+
+%Output the results to video:
+v = VideoWriter('C:\Group Project\Videos\output2');
+open(v)
+writeVideo(v,mov)
+close(v)
+
+end %main
