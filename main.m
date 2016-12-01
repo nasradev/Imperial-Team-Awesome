8a309aaa382ac785fc3f9c6bbf345dcc5c3e54a2
Radev <anr16@ic.ac.uk>
2016-12-01 16:42:21 +0000

    Added handling for multiple checkerboards but black misbehaving still

--- main.m
+++ main.m
@@ -14,7 +14,7 @@ obj = VideoReader('C:\Users\anr16\SharePoint\Gonzalez Bueno, Juana\Group project
 vidWidth = obj.Width;
 vidHeight = obj.Height;
 mov = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'), 'colormap',[]);
-k = 1;
+
 
 % Go through the video frames
 while hasFrame(obj);  
@@ -27,17 +27,29 @@ while hasFrame(obj);
      % Draw mask on the first cboard and find the next one
      temp_data = hideCheckerboard(data,...
          [firstBoard.imagePoints(1,:);firstBoard.imagePoints(end,:)]);
-    
+     
+     %Plot the points (TODO remove)
+     image(data);
+     hold on
+     scatter(firstBoard.imagePoints(:,1),firstBoard.imagePoints(:,2));
+     
      % Find the second board
      secondBoard = getBoardObject(temp_data, squareSize);
      
-    % Draw mask on the second cboard and find the next one
-     temp_data = hideCheckerboard(data,...
+     if secondBoard.imagePoints(1,1) > -1
+      
+      scatter(secondBoard.imagePoints(:,1),secondBoard.imagePoints(:,2));
+      
+      % Draw mask on the second cboard and find the next one
+      temp_data = hideCheckerboard(temp_data,...
          [secondBoard.imagePoints(1,:);secondBoard.imagePoints(end,:)]);
-     
-     % Find the third board (we expect 2 hands and a base):
-     thirdBoard = getBoardObject(temp_data, squareSize);
-    
+      % Find the third board (we expect 2 hands and a base):
+      thirdBoard = getBoardObject(temp_data, squareSize);
+      if thirdBoard.imagePoints(1,1) > -1
+       scatter(thirdBoard.imagePoints(:,1),thirdBoard.imagePoints(:,2));
+      end
+     end
+     hold off;
      % Get the marker positions:
      %markersPos = getMarkersPos(data);
      % Red and Yellow in tool 1, Green and Blue in tool 2
