42ee62af0636244229cd653638bdb22a30516a90
Radev <anr16@ic.ac.uk>
2016-12-05 14:56:38 +0000

    small changes

--- main.m
+++ main.m
@@ -10,11 +10,11 @@ squareSize = 5.4;
 load('naskosCameraParamsLaptop.mat') %or whatever
 
 %Set the video file and define output video object
-obj = VideoReader('C:\Users\anr16\SharePoint\Gonzalez Bueno, Juana\Group project\Aurora videos\6.MOV');
+obj = VideoReader('C:\Group Project\Videos\Take 2\IMG_5949.MOV');
 vidWidth = obj.Width;
 vidHeight = obj.Height;
 mov = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'), 'colormap',[]);
-
+k = 1;
 
 % Go through the video frames
 while hasFrame(obj);  
@@ -53,16 +53,22 @@ while hasFrame(obj);
      % Get the marker positions:
      tic
      [red yellow green blue] = getMarkerPos(data);
+     position = [50 50];
+     txt = strcat('Red: ', num2str(red(1,1)), ',', num2str(red(1,2)));
+     txt = strcat(txt, ' Yellow: ', num2str(yellow(1,1)), ',', num2str(yellow(1,2)));
+     data = insertText(data, position, txt, 'FontSize',18,'BoxColor',...
+     [0 0 255],'BoxOpacity',0.4,'TextColor','white');
      toc
      % Red and Yellow in tool 1, Green and Blue in tool 2
     else
         display('no checkerboards found')
     end
-    
+    mov(k).cdata = data;
+    k = k+1;
 end %hasFrame
 
 %Output the results to video:
-v = VideoWriter('C:\Group Project\Videos\output2');
+v = VideoWriter('C:\Group Project\Videos\output4');
 open(v)
 writeVideo(v,mov)
 close(v)
