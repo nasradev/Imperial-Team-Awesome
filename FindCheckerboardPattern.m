f53457c4751f96bf5c164b52cad38a5c26d5e03a
Radev <anr16@ic.ac.uk>
2016-12-06 14:25:11 +0000

    Can now detect blue checkerboard!

--- FindCheckerboardPattern.m
+++ FindCheckerboardPattern.m
@@ -9,19 +9,34 @@ function [imagePoints, worldPoints, colour, threePoints] = ...
     [imagePoints, boardSize] = detectCheckerboardPoints(data);
     % Ignore any patterns with fewer than 6 imagePoints as they
     % are likely to be artefacts
-    if (length(imagePoints) > 6) && (boardSize(1) > 0)
+    if (length(imagePoints) > 6) && (boardSize(1) > 0) && ...
+        (abs(max(imagePoints(:,1)) - min(imagePoints(:,1))) > 5) && ...
+        (abs(max(imagePoints(:,2)) - min(imagePoints(:,2))) > 5)
         worldPoints = generateCheckerboardPoints(boardSize, squareSize);
 
         % Set default colours to the first and second patterns detected.
         colour = black;
 
         % Find the the colour of the detected checkerboard
-        pointX = round(imagePoints(1,1) + (imagePoints(boardSize(1),1) - imagePoints(1,1)) / 2);
-        pointY = round(imagePoints(boardSize(1),2) + (imagePoints(boardSize(1),2) - imagePoints(1,2)) / 2);           
-        rgb = impixel(data,pointX,pointY);
-        if (rgb(3) > (rgb(2) + 10)) && (rgb(3) > (rgb(1) + 40))
+%         pointX = round(imagePoints(1,1) + (imagePoints(boardSize(1),1) - imagePoints(1,1)) / 2);
+%         pointY = round(imagePoints(boardSize(1),2) + (imagePoints(boardSize(1),2) - imagePoints(1,2)) / 2);           
+%         rgb = impixel(data,pointX,pointY);
+
+        pointX = round(imagePoints(1,1));
+        pointY = round(imagePoints(1,2));
+        pointX2 = round(imagePoints(5,1));
+        pointY2 = round(imagePoints(5,2));
+        
+        xrange = [min(pointX,pointX2):max(pointX,pointX2)];
+        yrange = [min(pointY,pointY2):max(pointY,pointY2)];
+        rgb(1) = max(min(data(yrange,xrange,1)));
+        rgb(2) = max(min(data(yrange,xrange,2)));
+        rgb(3) = max(min(data(yrange,xrange,3)));
+        rgb
+        
+        if (rgb(3) > (rgb(2) + 10)) && (rgb(3) > (rgb(1) + 10))
            colour = blue;
-        elseif (rgb(1) > (rgb(2) - 20)) && (rgb(1) > (rgb(3) - 40))
+        elseif (rgb(1) > (rgb(2) + 10)) && (rgb(1) > (rgb(3)+10))
            colour = red; %can also check for another colour here
         elseif (rgb(1) > 110) && (rgb(2) > 110) && (rgb(3) > 110)
            %handle white
